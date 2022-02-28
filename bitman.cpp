caa62df924f86a795ab54b6d85d6f13f51c7dc39
Anil Kumar Behera <ark845612@gmail.com.com>
2022-02-28 14:58:02 +0530

    unknown

--- bitman.cpp
+++ bitman.cpp
@@ -8,13 +8,18 @@ int setbit(int n,int pos){
     return (n|(1<<pos));
 }
 int clearbit(int n,int pos){
-    int notbitmask;
-    notbitmask=~(1<<pos);
-    return(notbitmask & n);
+    int mask=~(1<<pos);
+    return(mask & n);
+}
+int updatebit(int n,int pos,int value){
+    int mask=~(1<<pos);
+    n=n & mask;
+    return (n | (value<<pos));
 }
 int main(){
-    //cout<<getbit(5,2)<<endl;
+    // cout<<getbit(5,2)<<endl;
     //cout<<setbit(5,1)<<endl;
-    cout<<clearbit(5,2)<<endl;
+    // cout<<clearbit(5,2)<<endl;
+    cout<<updatebit(5,1,1)<<endl;
     return 0;
 }
